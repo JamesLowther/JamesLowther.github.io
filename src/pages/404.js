1313aa4e9908a4a9dc4c7d4d868d50f33b1ff7be
James Lowther <jamesevlowther@gmail.com>
2019-03-30 17:04:00 -0600

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
     <h1>PAGE NOT FOUND</h1>
-    <p>This page doesn&#39;t exist. <a href="/">Return home.</a></p>
+    <p>This page doesn&#39;t exist.</p>
+    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
