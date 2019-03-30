ab2a9857746c51630e83e2f5e8134a52e9bbfcdc
James Lowther <jamesevlowther@gmail.com>
2019-03-30 17:27:34 -0600

    remove intro

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,9 +15,8 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="https://github.com/JamesLowther/" target="_blank">Projects</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="https://github.com/JamesLowther/" target="_blank">Projects</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
