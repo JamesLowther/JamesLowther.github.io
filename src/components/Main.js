db24e286921abcb6d3e7de270ab0e54953f9eedc
James Lowther <jamesevlowther@gmail.com>
2019-05-12 01:38:13 -0600

    remove captcha

--- src/components/Main.js
+++ src/components/Main.js
@@ -30,7 +30,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact "method="post" data-netlify-recaptcha="true" action="/">
+          <form name="contact "method="post" action="/">
             <input type="hidden" name="form-name" value="contact" />
             <div className="field half first">
               <label htmlFor="name">Name</label>
@@ -44,7 +44,6 @@ class Main extends React.Component {
               <label htmlFor="message">Message</label>
               <textarea name="message" id="message" rows="4"></textarea>
             </div>
-            <div data-netlify-recaptcha="true"></div>
             <ul className="actions">
               <li><input type="submit" value="Send Message" className="special" /></li>
               <li><input type="reset" value="Reset" /></li>
