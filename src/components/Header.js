688516a85b857a836c60762fb8fb68f6c7984b63
James Lowther <jamesevlowther@gmail.com>
2020-01-01 19:49:58 -0700

    Changed project tab name

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = (props) => (
         <nav>
             <ul>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="https://github.com/JamesLowther/" target="_blank" rel="noopener noreferrer">Projects</a></li>
+                <li><a href="https://github.com/JamesLowther/" target="_blank" rel="noopener noreferrer">GitHub</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
