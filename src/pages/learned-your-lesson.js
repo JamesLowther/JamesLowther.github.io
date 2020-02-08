9d129eae4c960bf4ec62d861fd5a2aa29b1fe44c
James Lowther <jamesevlowther@gmail.com>
2020-02-07 20:10:49 -0700

    cruise ruse

---
+++ src/pages/learned-your-lesson.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import { Link } from 'gatsby'
+
+const LessonLearnedPage = () => (
+  <Layout>
+    <h1>IT WAS A RUSE THE WHOLE TIME!</h1>
+    <p>This has been an experiment in the masterful art of social engineering. I know this was convincing, but be more careful next time.</p>
+    <p>Good luck out there.</p>
+    <a href="https://en.wikipedia.org/wiki/Cruise_ship" target="_blank" rel="noopener noreferrer">Go back to the homepage</a>
+  </Layout>
+)
+
+export default LessonLearnedPage
