3f7a98d0519f63de3ac3211beaf7177b23b30ca4
James Lowther <jamesevlowther@gmail.com>
2020-02-07 20:11:15 -0700

    Update learned-your-lesson.js

--- src/pages/learned-your-lesson.js
+++ src/pages/learned-your-lesson.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 
 const LessonLearnedPage = () => (
   <Layout>
-    <h1>IT WAS A RUSE THE WHOLE TIME!</h1>
+    <h1>IT WAS A CRUISE RUSE THE WHOLE TIME!</h1>
     <p>This has been an experiment in the masterful art of social engineering. I know this was convincing, but be more careful next time.</p>
     <p>Good luck out there.</p>
     <a href="https://en.wikipedia.org/wiki/Cruise_ship" target="_blank" rel="noopener noreferrer">Go back to the homepage</a>
