6376d9451d6e436f1076dbb41196b246dfd1c0c3
James Lowther <jamesevlowther@gmail.com>
2020-10-21 14:08:26 -0600

    Changed image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `James Lowther`,
+    description: `Personal website for James Lowther`,
+    siteUrl: `https://www.jameslowther.com`,
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-google-fonts`,
