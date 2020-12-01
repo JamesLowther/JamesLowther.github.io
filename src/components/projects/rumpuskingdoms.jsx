d432943900d7044e9fcf14701c1c51276f887c1f
James Lowther <jamesevlowther@gmail.com>
2020-11-30 17:54:43 -0700

    Added data vis project

--- src/components/projects/rumpuskingdoms.jsx
+++ src/components/projects/rumpuskingdoms.jsx
@@ -24,7 +24,7 @@ const RumpusKingdomsProject = () => {
       />
       <div class="w-full md:w-2/3">
         <p class="text-white text-4xl font-header mb-8">Rumpus Kingdoms</p>
-        <p class="text-white text-2xl mb-8">Discord-driven attack-and-defend strategy game based around a medieval theme. Players would build a kingdom by settling villages. Attack and defence units could be purchased to attack other player's kingdoms. Complete with front-end scoreboard to track player status. Written with discord.py for the back-end and flask for the front-end website.</p>
+        <p class="text-white text-2xl mb-8">Discord-driven attack-and-defend strategy game based around a medieval theme. Players would build a kingdom by settling villages. Attack and defence units could be purchased to attack other player's kingdoms. Complete with front-end scoreboard to track player status. Written with <span class="text-green-400">discord.py</span> for the back-end and <span class="text-green-400">flask</span> for the front-end website.</p>
         <div class="text-center md:text-left">
           <a class="text-2xl text-white underline" href="https://github.com/JamesLowther/rumpus-kingdoms" target="_blank" rel="noreferrer">GitHub repo</a>
         </div>
