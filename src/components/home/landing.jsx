18faf83399c96dd54eee496617ff43eb40fe54a8
James Lowther <jamesevlowther@gmail.com>
2023-06-26 19:38:36 -0600

    fix ascii sharpness

--- src/components/home/landing.jsx
+++ src/components/home/landing.jsx
@@ -7,80 +7,80 @@ import Terminal from "./terminal";
 
 const Landing = () => {
   return (
-    <section id="landing" className="h-full">
-      <div className="flex items-center justify-center w-full py-4">
-        <div className="flex flex-row">
+    <section id="home" className="h-full">
+      <div className="h-1/6">
+        <div className="flex items-center justify-center w-full py-4">
+          <div className="flex flex-row">
+            <a
+              href="https://github.com/JamesLowther/"
+              target="_blank"
+              rel="noreferrer"
+              className="mx-6 transform hover:scale-103 transition duration-100 ease-out"
+              aria-label="Go to my GitHub page"
+            >
+              <StaticImage
+                src="../../assets/images/github.png"
+                className="w-16"
+                placeholder="blurred"
+                alt="GitHub logo"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/jameselowther/"
+              target="_blank"
+              rel="noreferrer"
+              className="mx-6 transform hover:scale-103 transition duration-100 ease-out"
+              aria-label="Go to my LinkedIn page"
+            >
+              <StaticImage
+                src="../../assets/images/linkedin.png"
+                className="w-16"
+                placeholder="blurred"
+                alt="LinkedIn logo"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="flex items-center justify-center w-full py-4">
           <a
-            href="https://github.com/JamesLowther/"
-            target="_blank"
-            rel="noreferrer"
-            className="mx-6 transform hover:scale-103 transition duration-100 ease-out"
-            aria-label="Go to my GitHub page"
+            href="https://blog.jameslowther.com"
+            className="font-header text-rose-700 font-bold text-2xl transform hover:scale-103 transition duration-75 ease-out animate-pulse "
           >
-            <StaticImage
-              src="../../assets/images/github.png"
-              className="w-16"
-              placeholder="blurred"
-              alt="GitHub logo"
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/jameselowther/"
-            target="_blank"
-            rel="noreferrer"
-            className="mx-6 transform hover:scale-103 transition duration-100 ease-out"
-            aria-label="Go to my LinkedIn page"
-          >
-            <StaticImage
-              src="../../assets/images/linkedin.png"
-              className="w-16"
-              placeholder="blurred"
-              alt="LinkedIn logo"
-            />
+            🚀 Check out my blog
           </a>
         </div>
       </div>
-      <div className="flex items-center justify-center w-full py-4">
-        <a
-          href="https://blog.jameslowther.com"
-          className="font-header text-rose-700 font-bold text-2xl transform hover:scale-103 transition duration-75 ease-out animate-pulse "
-        >
-          🚀 Check out my blog
-        </a>
-      </div>
-      <div className="lg:flex h-5/6">
-        <div className="flex flex-col lg:flex-row justify-around items-center">
-          <div className="flex items-center w-5/6 my-16 lg:m-4 lg:w-1/3 rounded-lg bg-gray-300">
-            <div className="px-10 py-10">
-              <p className="text-4xl tracking-widest font-header">
-                JAMES LOWTHER
-              </p>
-              <p className="text-xl font-display mt-2">
-                Graduate from the Univerity of Calgary with a BSc in Computer
-                Science. Interested in cybersecurity, cloud infrastructure,
-                DevOps principles, and front-end and back-end software
-                development.
-              </p>
-              <p className="flex flex-wrap items-center text-xl font-display mt-4">
-                <span className="mr-1">Calgary, AB, Canada</span>
-                <StaticImage
-                  src="../../assets/images/canadian-flag.png"
-                  className="shadow rounded-sm w-9 inline"
-                  placeholder="blurred"
-                  alt="Canadian flag"
-                />
-              </p>
-              <div className="text-lg mt-8">
-                <Terminal />
-              </div>
+      <div className="flex flex-col lg:flex-row justify-around items-center h-5/6 overflow-scroll pb-32">
+        <div className="flex items-center my-16 lg:m-24 w-5/6 lg:w-2/5 rounded-lg bg-gray-300">
+          <div className="p-10">
+            <p className="text-4xl tracking-widest font-header">
+              JAMES LOWTHER
+            </p>
+            <p className="text-xl font-display mt-2">
+              Graduate from the Univerity of Calgary with a BSc in Computer
+              Science. Interested in cybersecurity, cloud infrastructure,
+              DevOps principles, and front-end and back-end software
+              development.
+            </p>
+            <p className="flex flex-wrap items-center text-xl font-display mt-4">
+              <span className="mr-1">Calgary, AB, Canada</span>
+              <StaticImage
+                src="../../assets/images/canadian-flag.png"
+                className="shadow rounded-sm w-9 inline"
+                placeholder="blurred"
+                alt="Canadian flag"
+              />
+            </p>
+            <div className="text-lg mt-8">
+              <Terminal />
             </div>
           </div>
-          <ASCIIAnimation
-            source={iss}
-            className="flex items-center w-full h-full lg:w-7/12 mb-10"
-            frameDelay={40}
-          />
         </div>
+        <ASCIIAnimation
+          source={iss}
+          className="hidden sm:flex items-center h-full w-5/6 lg:w-3/5"
+          frameDelay={40}
+        />
       </div>
     </section>
   );
